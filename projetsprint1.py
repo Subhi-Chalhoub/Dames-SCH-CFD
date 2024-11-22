1d3fa1a5e96f68645a3c14ab8867a1065e2b8072
mitsuko-aka <subhi.chalhoub@eduvaud.ch>
2024-11-22 10:28:43 +0100

    update 100 case

--- projetsprint1.py
+++ projetsprint1.py
@@ -2,13 +2,15 @@ import pygame
 
 # Initialiser Pygame et la fenêtre
 pygame.init()
-fenetre = pygame.display.set_mode((1000, 100))
+fenetre = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption("sprint1")
 
 # Couleurs et paramètres
 BLANC, NOIR = (255, 255, 255), (0, 0, 0)
-TAILLE_CASE, NOMBRE_CASES = 100, 10
+TAILLE_CASE, NOMBRE_CASES = 80, 10
 position_pion = 0
+
+
 
 # Charger l'image du pion
 pion_image = pygame.image.load("MA-24_pion.png")
@@ -27,14 +29,22 @@ while True:
                 position_pion = min(position_pion + 1, NOMBRE_CASES - 1)  # Limite le pion au bord droit
             # Déplacement vers la gauche
             elif event.key == pygame.K_LEFT:
-                position_pion = max(position_pion - 1, 0)  # Limite le pion au bord gauche
+                position_pion = max(position_pion - 1, 0) # Limite le pion au bord gauche
+            if
 
     # Dessin de la ligne et du pion
     fenetre.fill(BLANC)
 
-    for i in range(NOMBRE_CASES):
-        couleur = NOIR if i % 2 == 0 else BLANC
-        pygame.draw.rect(fenetre, couleur, (i * TAILLE_CASE, 0, TAILLE_CASE, 100))
+    for ligne in range (10):
+        for colonne in range (10):
+            x = ligne * TAILLE_CASE
+            y = colonne * TAILLE_CASE
+            rectangle_case = pygame.Rect(x, y, TAILLE_CASE, TAILLE_CASE)
+            if (ligne + colonne) % 2 == 0:
+                pygame.draw.rect(fenetre, (0,0,0), rectangle_case)
+            else:
+                pygame.draw.rect(fenetre, (0, 0, 0), rectangle_case, 1)
+
 
     # Affiche l'image du pion à la position actuelle
     fenetre.blit(pion_image, (position_pion * TAILLE_CASE, 0))
